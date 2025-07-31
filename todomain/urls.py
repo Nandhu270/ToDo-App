00cc3b2ded424d71b8029c25f92bc3695e79b243
Nandhu270 <p.v.nandhakumar27@gmail.com>
2025-07-31 20:56:43 +0530

    feat:Added Undone Task Feature

--- todomain/urls.py
+++ todomain/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('index/',views.Home,name='index'),
-    path('add/',views.Add,name='add')
+    path('add/',views.Add,name='add'),
+    path('done/<int:id>/',views.Done,name='done'),
+    path('delete/<int:id>/',views.Delete,name='delete'),
+    path('edit/<int:id>/',views.Edit,name='edit'),
+    path('undone/<int:id>/',views.Undone,name='undone'),
 ]
